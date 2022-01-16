5ae1c9f21d456bb04ecb3c4a9a71b9914652a3e3
NOoBODDY <offmysoap@gmail.com>
2022-01-16 19:58:22 +0300

    DB fix. Added images and categories for products

--- InternetShop/Models/Product.cs
+++ InternetShop/Models/Product.cs
@@ -6,9 +6,10 @@
         public string FullName { get; set; }
         public string Shortname { get; set; }
         public string Description { get; set; }
-
+        public string ImagePath { get; set; }
         public Seller Seller { get; set; }
         public List<Tag> Tags { get; set; } = new();
         public List<ProductOnSell> Products { get; set; } = new();
+        public List<Category> Categories { get; set; } = new();
     }
 }
